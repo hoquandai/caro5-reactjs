6b0b56092fb92d0bc8a4217c9ac28328fee20ccc
hodai <hoquandai@gmail.com>
2019-10-22 20:26:45 +0700

    apply reacte router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// react-native
+// import { NativeRouter } from "react-router-native";
+
+// react-dom (what we'll use here)
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
-import Caro from "./components/Caro";
+// import Caro from "./components/Caro";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Caro />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your Caro to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
